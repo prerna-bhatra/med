467a160bebb0a314c46b97bfd34f669a6fd72a57
prerna <bhatraprerna061@gmail.com>
2020-03-19 19:48:40 +0530

    cartdone

--- showmed.php
+++ showmed.php
@@ -9,6 +9,7 @@
  	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<div class="container">
 <h1>Add to cart</h1>
 <?php
 	include 'database.php';
@@ -24,15 +25,39 @@
 <div class="container">
 <p id="medname"><?php echo $output['name']; ?></p>
 <p id="medname"><?php echo "Rs.".$output['price']; ?></p>
-<div>
-<div id="cart">
-<button class="btn" ><b>-</b></button>
-<?php echo $num ?>
-<button class="btn"><b>+</b></button>
-</div>
 
+	<div id="cart">
+		<button class="btn" onclick="dec()"><b>-</b></button>
+		<p id="num"></p>
+		<button class="btn" onclick="inc()"><b>+</b></button>
+	</div>
+
+
+
+<script type="text/javascript">
+	var num=1;
+	function dec()
+	{
+		if(num>1)
+		{
+			num=num-1;
+			document.getElementById('num').innerHTML=num;
+		}		
+	}
+	function inc()
+	{
+		num=num+1;
+			document.getElementById('num').innerHTML=num;
+	}
+	document.getElementById('num').innerHTML=num;
+</script>
 </div>
 </body>
 </html>
+
+
+
+
+
 
 
