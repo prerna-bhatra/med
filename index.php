9ae37da08518d1807e1ceceadc569d2ef9676fc3
prerna <bhatraprerna061@gmail.com>
2020-03-30 11:19:48 +0530

    slide

--- index.php
+++ index.php
@@ -99,16 +99,16 @@
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
          <li><input type="text" class="form-control" name="search" placeholder="search medicine" onkeyup = "showHint(this.value)"></li>
-        <li class="dropdown">
+        <!--<li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="#">Page 1-1</a></li>
             <li><a href="#">Page 1-2</a></li>
             <li><a href="#">Page 1-3</a></li>
           </ul>
-        </li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
+        </li>-->
+        <li><a href="cart.php">Cart</a></li>
+        <li><a href="#">About Us</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
       <?php
@@ -133,6 +133,60 @@
     <div id="txtHint">
         
     </div>
+</div>
+<div class="slide">
+  <div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="doc1.jpg" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Ms yang</h3>
+          <p>The best doctor gives least medicine.</p>
+        </div>
+      </div>
+
+      
+      <div class="item">
+        <img src="doc1.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Ms smita</h3>
+          <p>Whereever the art of medcine is loved there is also a love for hummanity.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="doc2.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Ms prerna</h3>
+          <p>Doctors are boon sent by god for us.</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 </div>
 </body>
 </html>
